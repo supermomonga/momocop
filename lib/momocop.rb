f2009a08174fb5af2582aa81c8012cf0e3d58996
supermomonga <hi@supermomonga.com>
2024-02-11 17:59:22 +0900

    init

---
+++ lib/momocop.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "momocop/version"
+
+module Momocop
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/momocop_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Momocop do
+  it "has a version number" do
+    expect(Momocop::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
